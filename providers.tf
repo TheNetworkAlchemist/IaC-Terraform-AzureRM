bf1853451fec73bd63d23f503c25002e341b499c
CHobbes007 <30755959+CHobbes007@users.noreply.github.com>
2022-02-18 16:58:13 -0600

    Added Authentications Methods

--- providers.tf
+++ providers.tf
@@ -10,4 +10,11 @@ terraform {
 # Configure the Microsoft Azure Provider
 provider "azurerm" {
   features {}
+
+  subscription_id = var.azure-subscription-id
+  client_id       = var.azure-client-id
+  # client_secret                     = "..."
+  arm-client-certificate-path     = var.arm-client-certificate-path
+  arm-client-certificate-password = var.arm-client-certificate-password
+  tenant_id                       = var.azure-tenant-id
 }
